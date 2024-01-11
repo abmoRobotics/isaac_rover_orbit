8a721f4408639fd8a3341d07f109fc7d45303080
abmoRobotics <abmo19@student.aau.dk>
2024-01-11 13:15:58 +0100

    Removed ORBIT v0.1.0 files

--- rover_envs/envs/rover_new/config/aau_rover/__init__.py
+++
@@ -1,3 +0,0 @@
-# import gymnasium as gym
-
-# from . import agents, env_cfg
--- rover_envs/envs/rover_new/mdp/__init__.py
+++
@@ -1,7 +0,0 @@
-from omni.isaac.orbit.envs.mdp import *  # noqa: F401, F403
-
-from .actions import *  # noqa: F401, F403
-from .observations import *  # noqa: F401, F403
-from .randomizations import *
-from .rewards import *  # noqa: F401, F403
-from .terminations import *  # noqa: F401, F403
