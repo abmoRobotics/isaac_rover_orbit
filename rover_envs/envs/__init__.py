35a87121d5865be60c781b77c2088763f17f0e3c
abmoRobotics <abmo19@student.aau.dk>
2024-01-11 14:50:33 +0100

    Refactor Rover environment imports

--- rover_envs/envs/__init__.py
+++ rover_envs/envs/__init__.py
@@ -2,32 +2,25 @@ import os
 
 import gymnasium as gym
 
-from .rover_new import rover_env_cfg
+from .rover import rover_env_cfg
 
 ORBIT_CUSTOM_ENVS_EXT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 
 ORBIT_CUSTOM_ENVS_DATA_DIR = os.path.join(ORBIT_CUSTOM_ENVS_EXT_DIR, "data")
 
-gym.register(
-    id="Rover-v0",
-    entry_point="envs.rover:RoverEnv",
-    kwargs={"cfg_entry_point": "envs.rover:RoverEnvCfg"},
-)
+
 
 gym.register(
-    id="RoverCamera-v0",
-    entry_point="envs.rover:RoverEnvCamera",
-    kwargs={"cfg_entry_point": "envs.rover:RoverEnvCfg"},
-)
-
-#from rover_new import rover_env_cfg
-
-gym.register(
-    id='RoverNew-v0',
-    # entry_point='omni.isaac.orbit.envs:RLTaskEnv',
-    entry_point='rover_envs.envs.rover_new:RoverEnv',
-    #disable_env_checker=True,
+    id='Rover-v0',
+    entry_point='omni.isaac.orbit.envs:RLTaskEnv',
+    #entry_point='rover_envs.envs.rover_new:RoverEnv',
+    disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": "rover_envs.envs.rover_new:RoverEnvCfg",
+        "env_cfg_entry_point": "rover_envs.envs.rover:RoverEnvCfg",
     }
 )
+# gym.register(
+#     id="RoverCamera-v0",
+#     entry_point="envs.rover:RoverEnvCamera",
+#     kwargs={"cfg_entry_point": "envs.rover:RoverEnvCfg"},
+# )
