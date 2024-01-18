fd7e7c426ae9f20688c95c016e80e99649196406
abmoRobotics <abmo19@student.aau.dk>
2024-01-18 15:58:24 +0100

    Added rewards to self.extras["episodes"] in addition to self.extras["log"] for SKRL logging purposes.

--- rover_envs/envs/rover/rover_env.py
+++ rover_envs/envs/rover/rover_env.py
@@ -20,3 +20,17 @@ class RoverEnv(RLTaskEnv):
         terrain: TerrainImporter = self.scene.terrain
         terrain.env_origins[env_ids, 0] += 100
         terrain.env_origins[env_ids, 1] += 100
+
+    def _reset_idx(self, idx: torch.Tensor):
+        """Reset the environment at the given indices.
+
+        Note:
+            This function inherits from :meth:`omni.isaac.orbit.envs.rl_task_env.RLTaskEnv._reset_idx`.
+            This is done because SKRL requires the "episode" key in the extras dict to be present in order to log.
+        Args:
+            idx (torch.Tensor): Indices of the environments to reset.
+        """
+        super()._reset_idx(idx)
+
+        # Done this way because SKRL requires the "episode" key in the extras dict to be present in order to log.
+        self.extras["episode"] = self.extras["log"]
