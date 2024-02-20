473e55f3fd4a7c8eca3ee18307acd92de2e121a2
Anton Bjørndahl Mortensen <56405924+abmoRobotics@users.noreply.github.com>
2024-02-20 09:30:56 +0100

    Upgrades to SKRL 1.1 and adds new agents (#8)
    * Removed collision sensor filter from rover_env
    
    * Formatting
    
    * Added rewards to self.extras["episodes"] in addition to self.extras["log"] for SKRL logging purposes.
    
    * Fixed angle to target and minor formatting
    
    * Fixed angle to target and minor formatting
    
    * Fixed angle to target and minor formatting
    
    * Added example with no obstacles
    
    * Updated docs
    
    * Changed file path name
    
    * Added debug terrain
    
    * Added mars terrain
    
    * Added recorder
    
    * Added Custom Command visualizer
    
    * Fix configuration
    
    * Added new reward and fixed scaling
    
    * Fixed configuration
    
    * Formatting
    
    * Added Dockerfile
    
    * skrl fix
    
    * debug terrain
    
    * added usd as lfs
    
    * added merged usd terrain
    
    * added terrain
    
    * added robot
    
    * Changed docker compose
    
    * Added scripts for docker
    
    * temp fix
    
    * Added .env
    
    * Updated Readme
    
    * Removed bugged USDs
    
    * branch change
    
    * Added USD downloads
    
    * Changed git ignore
    
    * Changed download_usd
    
    * temp commit
    
    * temp commitø
    
    * Temp commit
    
    * SAC
    
    * SAC
    
    * Fixes terrain bug
    
    * Border offset changed
    
    * fix
    
    * Changes
    
    * Settings
    
    * change
    
    * Fixes
    
    * Changes to the framework
    
    * Upgrades to SKRL 1.1 and adds TRPO, RPO, TD3, SAC, and cleans up

---
+++ rover_envs/learning/train/__init__.py
@@ -0,0 +1,31 @@
+from gym.spaces import Box
+from omni.isaac.orbit.envs import RLTaskEnv
+
+from rover_envs.learning.train.ppo import PPO_agent
+from rover_envs.learning.train.rpo import RPO_agent
+from rover_envs.learning.train.sac import SAC_agent
+from rover_envs.learning.train.td3 import TD3_agent
+from rover_envs.learning.train.trpo import TRPO_agent
+
+
+def get_agent(agent: str, env: RLTaskEnv, observation_space: Box, action_space: Box, experiment_cfg):
+    """
+    Function to get the agent.
+
+    Args:
+        agent (str): The agent.
+
+    Returns:
+        Agent: The agent.
+    """
+    if agent == "PPO":
+        return PPO_agent(experiment_cfg, observation_space, action_space, env)
+    if agent == "TRPO":
+        return TRPO_agent(experiment_cfg, observation_space, action_space, env)
+    if agent == "RPO":
+        return RPO_agent(experiment_cfg, observation_space, action_space, env)
+    if agent == "TD3":
+        return TD3_agent(experiment_cfg, observation_space, action_space, env)
+    if agent == "SAC":
+        return SAC_agent(experiment_cfg, observation_space, action_space, env)
+    raise ValueError(f"Agent {agent} not supported.")
---
+++ rover_envs/learning/train/get_models.py
@@ -0,0 +1,142 @@
+
+from gymnasium.spaces.box import Box
+from omni.isaac.orbit.envs import RLTaskEnv
+
+from rover_envs.envs.rover.learning.models import (Critic, DeterministicActor, DeterministicNeuralNetwork,
+                                                   GaussianNeuralNetwork)
+
+
+def get_models(agent: str, env: RLTaskEnv, observation_space: Box, action_space: Box):
+    """
+    Placeholder function for getting the models.
+
+    Note:
+        This function will be further improved in the future, by reading the model config from the experiment config.
+
+    Args:
+        agent (str): The agent.
+
+    Returns:
+        dict: A dictionary containing the models.
+    """
+
+    if agent == "PPO":
+        return get_model_gaussian(env, observation_space, action_space)
+    if agent == "TRPO":
+        return get_model_gaussian(env, observation_space, action_space)
+    if agent == "RPO":
+        return get_model_gaussian(env, observation_space, action_space)
+    if agent == "SAC":
+        return get_model_double_critic_deterministic(env, observation_space, action_space)
+    if agent == "TD3":
+        return get_model_double_critic_deterministic(env, observation_space, action_space)
+
+    raise ValueError(f"Agent {agent} not supported.")
+
+
+def get_model_gaussian(env: RLTaskEnv, observation_space: Box, action_space: Box):
+    models = {}
+    encoder_input_size = env.observation_manager.group_obs_term_dim["policy"][-1][0]
+
+    mlp_input_size = 4
+
+    models["policy"] = GaussianNeuralNetwork(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+    models["value"] = DeterministicNeuralNetwork(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+    return models
+
+
+def get_model_double_critic_deterministic(env: RLTaskEnv, observation_space: Box, action_space: Box):
+    models = {}
+    encoder_input_size = env.observation_manager.group_obs_term_dim["policy"][-1][0]
+
+    mlp_input_size = 4
+
+    models["policy"] = DeterministicActor(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+    models["target_policy"] = DeterministicActor(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+
+    models["critic_1"] = Critic(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+    models["critic_2"] = Critic(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+    models["target_critic_1"] = Critic(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+    models["target_critic_2"] = Critic(
+        observation_space=observation_space,
+        action_space=action_space,
+        device=env.device,
+        mlp_input_size=mlp_input_size,
+        mlp_layers=[256, 160, 128],
+        mlp_activation="leaky_relu",
+        encoder_input_size=encoder_input_size,
+        encoder_layers=[80, 60],
+        encoder_activation="leaky_relu",
+    )
+
+    return models
